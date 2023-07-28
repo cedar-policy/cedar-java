1960d93644e73ef14d9d6b31bf7dcdaafa011f18
Andrew Wells <anmwells@amazon.com>
2023-07-27 17:20:38 -0700

    fix cedar-integration-tests path

--- CedarJava/config.sh
+++ CedarJava/config.sh
@@ -8,12 +8,16 @@ ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$parent_dir"/CedarJavaFFI/t
 else
 ffi_lib_str="    environment 'CEDAR_JAVA_FFI_LIB', '"$parent_dir"/CedarJavaFFI/target/debug/libcedar_java_ffi.so'"
 fi
-
 sed "83s;.*;$ffi_lib_str;" "build.gradle" > new_build.gradle
 mv new_build.gradle build.gradle
+
+int_tests_str="    environment 'CEDAR_INTEGRATION_TESTS_ROOT', '"$parent_dir"/cedar/cedar-integration-tests'"
+sed "82s;.*;$int_test_str;" "build.gradle" > new_build.gradle
+mv new_build.gradle build.gradle
+
 export MUST_RUN_CEDAR_INTEGRATION_TESTS=1
 
-curr_dir="$(pwd)"
-export CEDAR_INTEGRATION_TESTS_ROOT=$curr_dir/cedar/cedar-integration-tests
+tail build.gradle
 
-echo $CEDAR_INTEGRATION_TESTS_ROOT
+ls ..
+ls ../..
