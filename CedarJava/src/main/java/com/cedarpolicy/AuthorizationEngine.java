7b458ce2975fb74e4e86855cce3aac8df30da8fe
Kesha Hietala <khieta@amazon.com>
2023-10-26 11:53:46 -0400

    bump to 3.0

--- CedarJava/src/main/java/com/cedarpolicy/AuthorizationEngine.java
+++ CedarJava/src/main/java/com/cedarpolicy/AuthorizationEngine.java
@@ -68,6 +68,6 @@ public interface AuthorizationEngine {
      * @return The Cedar language major version supported
      */
     static String getCedarLangVersion() {
-        return "2.3";
+        return "3.0";
     }
 }
--- CedarJavaFFI/src/interface.rs
+++ CedarJavaFFI/src/interface.rs
@@ -95,7 +95,7 @@ pub fn callCedarJNI(
 /// The main JNI entry point
 #[jni_fn("com.cedarpolicy.BasicAuthorizationEngine")]
 pub fn getCedarJNIVersion(env: JNIEnv<'_>) -> jstring {
-    env.new_string("2.3")
+    env.new_string("3.0")
         .expect("error creating Java string")
         .into_raw()
 }
