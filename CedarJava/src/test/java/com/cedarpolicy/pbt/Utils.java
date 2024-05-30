728e52426774603496c5891fc27eddc9730a417a
Andrew Wells <anmwells@amazon.com>
2024-05-30 16:31:47 -0700

    Add 'is' to reserved words for property tests
    Signed-off-by: Andrew Wells <anmwells@amazon.com>

--- CedarJava/src/test/java/com/cedarpolicy/pbt/Utils.java
+++ CedarJava/src/test/java/com/cedarpolicy/pbt/Utils.java
@@ -36,6 +36,7 @@ public final class Utils {
                             "unless",
                             "advice",
                             "in",
+                            "is",
                             "has",
                             "if",
                             "then",
