de0c8b64623ead74f11943952bdacd54c8679b41
Felix Zheng <37223155+felixzheng98@users.noreply.github.com>
2024-07-18 13:51:02 -0400

    Add support for cedar-policy-formatter (#180)
    Signed-off-by: Felix Zheng <felix.zheng@mongodb.com>

---
+++ CedarJava/src/test/java/com/cedarpolicy/PolicyFormatterTests.java
@@ -0,0 +1,40 @@
+package com.cedarpolicy;
+
+import com.cedarpolicy.formatter.PolicyFormatter;
+import com.cedarpolicy.model.exception.InternalException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PolicyFormatterTests {
+
+  private static final String TEST_RESOURCES_DIR = "src/test/resources/";
+
+  @Test
+  public void testPoliciesStrToPretty() throws Exception {
+    String unformattedCedarPolicy = Files.readString(
+        Path.of(TEST_RESOURCES_DIR + "unformatted_policy.cedar"));
+
+    String formattedCedarPolicy = Files.readString(
+        Path.of(TEST_RESOURCES_DIR + "formatted_policy.cedar"));
+
+    assertEquals(formattedCedarPolicy, PolicyFormatter.policiesStrToPretty(unformattedCedarPolicy));
+  }
+
+  @Test
+  public void testPoliciesStrToPrettyMalformedCedarPolicy() throws Exception {
+    String malformedCedarPolicy = Files.readString(
+        Path.of(TEST_RESOURCES_DIR + "malformed_policy_set.cedar"));
+
+    assertThrows(InternalException.class,
+        () -> PolicyFormatter.policiesStrToPretty(malformedCedarPolicy));
+  }
+
+  @Test
+  public void testPoliciesStrToPrettyNullSafety() {
+    assertThrows(NullPointerException.class, () -> PolicyFormatter.policiesStrToPretty(null));
+  }
+}
