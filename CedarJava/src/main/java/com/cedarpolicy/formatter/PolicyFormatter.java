80f8077685d4c4158dc2259b8fc358b1108f63a1
Felix Zheng <37223155+felixzheng98@users.noreply.github.com>
2024-10-16 13:39:24 -0400

    Add support for specifying formatter config (#235)
    Signed-off-by: Felix Zheng <felix.zheng@mongodb.com>

--- CedarJava/src/main/java/com/cedarpolicy/formatter/PolicyFormatter.java
+++ CedarJava/src/main/java/com/cedarpolicy/formatter/PolicyFormatter.java
@@ -2,6 +2,7 @@ package com.cedarpolicy.formatter;
 
 import com.cedarpolicy.loader.LibraryLoader;
 import com.cedarpolicy.model.exception.InternalException;
+import com.cedarpolicy.model.formatter.Config;
 
 public final class PolicyFormatter {
 
@@ -13,5 +14,8 @@ public final class PolicyFormatter {
   }
 
   public static native String policiesStrToPretty(String policies)
+      throws InternalException, NullPointerException;
+
+  public static native String policiesStrToPrettyWithConfig(String policies, Config config)
       throws InternalException, NullPointerException;
 }
