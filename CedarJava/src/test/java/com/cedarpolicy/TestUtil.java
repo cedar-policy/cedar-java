19b9b6a4ca21aeb543d82c1f9ca7bcc001ff4326
Mudit Chaudhary <chmudit@amazon.com>
2025-01-24 17:53:31 +0000

    adds .toJson unit tests for PolicySet and corresponding test utils
    Signed-off-by: Mudit Chaudhary <chmudit@amazon.com>

--- CedarJava/src/test/java/com/cedarpolicy/TestUtil.java
+++ CedarJava/src/test/java/com/cedarpolicy/TestUtil.java
@@ -18,11 +18,21 @@ package com.cedarpolicy;
 
 import com.cedarpolicy.model.schema.Schema;
 import com.cedarpolicy.model.schema.Schema.JsonOrCedar;
+import com.cedarpolicy.model.policy.TemplateLink;
+import com.cedarpolicy.model.policy.PolicySet;
+import com.cedarpolicy.model.policy.LinkValue;
+import com.cedarpolicy.model.policy.Policy;
+import com.cedarpolicy.model.entity.Entity;
+import com.cedarpolicy.value.EntityTypeName;
 
+import java.util.HashSet;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 /** Utils to help with tests. */
 public final class TestUtil {
@@ -45,4 +55,57 @@ public final class TestUtil {
             throw new RuntimeException("Failed to load test schema file " + schemaFile, e);
         }
     }
+
+    public static PolicySet buildValidPolicySet() {
+        EntityTypeName principalType = EntityTypeName.parse("User").get();
+        Set<Policy> policies = new HashSet<>();
+        Set<Policy> templates = new HashSet<>();
+        ArrayList<TemplateLink> templateLinks = new ArrayList<TemplateLink>(); 
+        ArrayList<LinkValue> linkValueList = new ArrayList<>();
+
+        String fullPolicy =
+                "permit(principal == User::\"Bob\", action == Action::\"View_Photo\", resource in Album::\"Vacation\");";
+        Policy newPolicy = new Policy(fullPolicy, "p1");
+        policies.add(newPolicy);
+
+        String template = "permit(principal == ?principal, action == Action::\"View_Photo\", resource in Album::\"Vacation\");";
+        Policy policyTemplate = new Policy(template, "t0");
+        templates.add(policyTemplate);
+
+        Entity principal = new Entity(principalType.of("Alice"), new HashMap<>(), new HashSet<>());
+        LinkValue principalLinkValue = new LinkValue("?principal", principal.getEUID());
+        linkValueList.add(principalLinkValue);
+
+        TemplateLink templateLink = new TemplateLink("t0", "tl0", linkValueList);
+        templateLinks.add(templateLink);
+
+        return new PolicySet(policies, templates, templateLinks);
+    }
+
+    public static PolicySet buildInvalidPolicySet() {
+        EntityTypeName principalType = EntityTypeName.parse("User").get();
+        Set<Policy> policies = new HashSet<>();
+        Set<Policy> templates = new HashSet<>();
+        ArrayList<TemplateLink> templateLinks = new ArrayList<TemplateLink>(); 
+        ArrayList<LinkValue> linkValueList = new ArrayList<>();
+
+        String fullPolicy =
+                "permit(prinipal == User::\"Bob\", action == Action::\"View_Photo\", resource in Album::\"Vacation\");";
+        Policy newPolicy = new Policy(fullPolicy, "p1");
+        policies.add(newPolicy);
+
+        String template = "permit(principal, action == Action::\"View_Photo\", resource in Album::\"Vacation\");";
+        Policy policyTemplate = new Policy(template, "t0");
+        templates.add(policyTemplate);
+
+        Entity principal = new Entity(principalType.of("Alice"), new HashMap<>(), new HashSet<>());
+        LinkValue principalLinkValue = new LinkValue("?principal", principal.getEUID());
+        linkValueList.add(principalLinkValue);
+
+        TemplateLink templateLink = new TemplateLink("t0", "tl0", linkValueList);
+        templateLinks.add(templateLink);
+
+        return new PolicySet(policies, templates, templateLinks);
+    }
+
 }
