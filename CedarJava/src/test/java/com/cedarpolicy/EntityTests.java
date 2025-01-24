6293cd2b702976f0053a0f61b27deb3c8ecf7061
Mudit Chaudhary <chmudit@amazon.com>
2025-01-24 12:55:02 -0500

    Adds getter methods for PolicySet and Entity (#282)
    Signed-off-by: Mudit Chaudhary <chmudit@amazon.com>

---
+++ CedarJava/src/test/java/com/cedarpolicy/EntityTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Cedar Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package com.cedarpolicy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.cedarpolicy.value.*;
+import com.cedarpolicy.model.entity.Entity;
+
+
+public class EntityTests {
+
+    @Test
+    public void getAttrTests() {
+        PrimString stringAttr = new PrimString("stringAttrValue");
+        HashMap<String, Value> attrs = new HashMap<>();
+        attrs.put("stringAttr", stringAttr);
+        EntityTypeName principalType = EntityTypeName.parse("User").get();
+        Entity principal = new Entity(principalType.of("Alice"), attrs, new HashSet<>());
+
+        // Test valid attribute key and value
+        assertEquals(principal.getAttr("stringAttr"), stringAttr);
+
+        // Test invalid attribute key
+        assertThrows(IllegalArgumentException.class, () -> {
+            principal.getAttr(null);
+        });
+
+        // Test key not found
+        assertEquals(principal.getAttr("decimalAttr"), null);
+    }
+}
