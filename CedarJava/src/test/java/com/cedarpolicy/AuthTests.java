317c807b51dc7b4751492de0b4eaa5c4431a0a62
Mudit Chaudhary <chmudit@amazon.com>
2025-01-28 21:55:50 +0000

    adds tests for backward compatible Context for isAuthorized
    Signed-off-by: Mudit Chaudhary <chmudit@amazon.com>

--- CedarJava/src/test/java/com/cedarpolicy/AuthTests.java
+++ CedarJava/src/test/java/com/cedarpolicy/AuthTests.java
@@ -27,11 +27,13 @@ import com.cedarpolicy.model.AuthorizationSuccessResponse.Decision;
 import com.cedarpolicy.model.exception.MissingExperimentalFeatureException;
 import com.cedarpolicy.model.entity.Entity;
 import com.cedarpolicy.model.policy.Policy;
+import com.cedarpolicy.model.Context;
 import com.cedarpolicy.model.policy.PolicySet;
 import com.cedarpolicy.value.EntityTypeName;
 import com.cedarpolicy.value.EntityUID;
 import com.cedarpolicy.value.Unknown;
 import com.cedarpolicy.value.Value;
+import com.cedarpolicy.value.PrimBool;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -52,6 +54,7 @@ public class AuthTests {
         });
     }
 
+
     @Test
     public void simple() {
         var alice = new EntityUID(EntityTypeName.parse("User").get(), "alice");
@@ -61,6 +64,66 @@ public class AuthTests {
         policies.add(new Policy("permit(principal,action,resource);", "p0"));
         var policySet = new PolicySet(policies);
         assertAllowed(q, policySet, new HashSet<>());
+    }
+
+    private Set<Entity> buildEntitiesForContextTests() {
+        EntityTypeName principalType = EntityTypeName.parse("User").get();
+        EntityTypeName actionType = EntityTypeName.parse("Action").get();
+        EntityTypeName albumResourceType = EntityTypeName.parse("Album").get();
+        EntityTypeName photoResourceType = EntityTypeName.parse("Photo").get();
+
+        Set<EntityUID> parents = new HashSet<>();
+        Entity album = new Entity(albumResourceType.of("Vacation"), new HashMap<>(), new HashSet<>());
+        parents.add(album.getEUID());
+        Entity photo = new Entity(photoResourceType.of("pic01"), new HashMap<>(), parents);
+
+        Set<Entity> entities = new HashSet<>();
+        entities.add(photo);
+        entities.add(album);
+        entities.add(new Entity(principalType.of("Alice"), new HashMap<>(), new HashSet<>()));
+        entities.add(new Entity(actionType.of("View_Photo"), new HashMap<>(), new HashSet<>()));
+
+        return entities;
+    }
+
+    private PolicySet buildPolicySetForContextTests() {
+        Set<Policy> ps = new HashSet<>();
+        String fullPolicy =
+                "permit(principal == User::\"Alice\", action == Action::\"View_Photo\", resource in Album::\"Vacation\")"
+                + "when {context.authenticated == true};";
+
+        Policy newPolicy = new Policy(fullPolicy, "p1");
+        ps.add(newPolicy);
+        return new PolicySet(ps);
+    }
+
+    @Test
+    public void authWithBackwardCompatibleContext() {
+        EntityUID principal = new EntityUID(EntityTypeName.parse("User").get(), "Alice");
+        EntityUID action = new EntityUID(EntityTypeName.parse("Action").get(), "View_Photo");
+        EntityUID resource = new EntityUID(EntityTypeName.parse("Photo").get(), "pic01");
+        Set<Entity> entities = buildEntitiesForContextTests();
+        PolicySet policySet = buildPolicySetForContextTests();
+        Map<String, Value> context = new HashMap<>();
+        context.put("authenticated", new PrimBool(true));
+        AuthorizationRequest r = new AuthorizationRequest(principal, action, resource, context);
+
+        assertAllowed(r, policySet, entities);
+    }
+
+    @Test
+    public void authWithContextObject() {
+        EntityUID principal = new EntityUID(EntityTypeName.parse("User").get(), "Alice");
+        EntityUID action = new EntityUID(EntityTypeName.parse("Action").get(), "View_Photo");
+        EntityUID resource = new EntityUID(EntityTypeName.parse("Photo").get(), "pic01");
+        Set<Entity> entities = buildEntitiesForContextTests();
+        PolicySet policySet = buildPolicySetForContextTests();
+        Map<String, Value> contextMap = new HashMap<>();
+        contextMap.put("authenticated", new PrimBool(true));
+        Context context = new Context(contextMap);
+        AuthorizationRequest r = new AuthorizationRequest(principal, action, resource, context);
+        
+        assertAllowed(r, policySet, entities);
     }
 
     @Test
