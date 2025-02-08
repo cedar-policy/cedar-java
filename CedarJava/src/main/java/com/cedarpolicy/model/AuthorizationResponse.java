fe1d1318b1fe1206aa054f5ae61c5b6ce15e9ff8
Ed Hillmann <edhillmann@yahoo.com>
2025-02-08 22:35:28 +1000

    Changes, but using ConcurrentHashMap for thread safety
    Signed-off-by: Ed Hillmann <edhillmann@yahoo.com>

--- CedarJava/src/main/java/com/cedarpolicy/model/AuthorizationResponse.java
+++ CedarJava/src/main/java/com/cedarpolicy/model/AuthorizationResponse.java
@@ -18,8 +18,8 @@ package com.cedarpolicy.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -34,10 +34,10 @@ public final class AuthorizationResponse {
     public final Optional<AuthorizationSuccessResponse> success;
     /** This will be present if and only if `type` is `Failure`. */
     @JsonProperty("errors")
-    public final Optional<ImmutableList<DetailedError>> errors;
+    public final Optional<List<DetailedError>> errors;
     /** Warnings can be produced regardless of whether we have a `Success` or `Failure`. */
     @JsonProperty("warnings")
-    public final ImmutableList<String> warnings;
+    public final List<String> warnings;
 
     /**
      * If `type` is `Success`, `success` should be present and `errors` empty.
@@ -52,11 +52,11 @@ public final class AuthorizationResponse {
     ) {
         this.type = type;
         this.success = success;
-        this.errors = errors.map((list) -> ImmutableList.copyOf(list));
+        this.errors = errors.map((list) -> List.copyOf(list));
         if (warnings == null) {
-            this.warnings = ImmutableList.of(); // empty
+            this.warnings = List.of(); // empty
         } else {
-            this.warnings = ImmutableList.copyOf(warnings);
+            this.warnings = List.copyOf(warnings);
         }
     }
 
