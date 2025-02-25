ef341fdf6ed9e7d7ef73ef11d4ef660988d5f1e9
Mudit Chaudhary <chmudit@amazon.com>
2025-02-25 09:27:41 -0500

    Adds Entities with backward compatibility (#293)
    Signed-off-by: Mudit Chaudhary <chmudit@amazon.com>

--- CedarJava/src/main/java/com/cedarpolicy/BasicAuthorizationEngine.java
+++ CedarJava/src/main/java/com/cedarpolicy/BasicAuthorizationEngine.java
@@ -20,14 +20,21 @@ import static com.cedarpolicy.CedarJson.objectReader;
 import static com.cedarpolicy.CedarJson.objectWriter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 import com.cedarpolicy.loader.LibraryLoader;
-import com.cedarpolicy.model.*;
+import com.cedarpolicy.model.AuthorizationResponse;
+import com.cedarpolicy.model.EntityValidationRequest;
+import com.cedarpolicy.model.PartialAuthorizationResponse;
+import com.cedarpolicy.model.ValidationRequest;
+import com.cedarpolicy.model.ValidationResponse;
+import com.cedarpolicy.model.entity.Entities;
+import com.cedarpolicy.model.entity.Entity;
 import com.cedarpolicy.model.exception.AuthException;
 import com.cedarpolicy.model.exception.BadRequestException;
 import com.cedarpolicy.model.exception.InternalException;
 import com.cedarpolicy.model.exception.MissingExperimentalFeatureException;
-import com.cedarpolicy.model.entity.Entity;
 import com.cedarpolicy.model.policy.PolicySet;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -35,10 +42,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-import java.util.List;
-import java.util.Set;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /** An authorization engine that is compiled in process. Communicated with via JNI. */
 public final class BasicAuthorizationEngine implements AuthorizationEngine {
@@ -57,6 +62,15 @@ public final class BasicAuthorizationEngine implements AuthorizationEngine {
         return call("AuthorizationOperation", AuthorizationResponse.class, request);
     }
 
+    /**
+     * Overloaded method to accept Entities object
+     */
+    @Override
+    public AuthorizationResponse isAuthorized(com.cedarpolicy.model.AuthorizationRequest q,
+                                              PolicySet policySet, Entities entities) throws AuthException {
+        return isAuthorized(q, policySet, entities.getEntities());
+    }
+
     @Experimental(ExperimentalFeature.PARTIAL_EVALUATION)
     @Override
     public PartialAuthorizationResponse isAuthorizedPartial(com.cedarpolicy.model.PartialAuthorizationRequest q,
@@ -71,6 +85,16 @@ public final class BasicAuthorizationEngine implements AuthorizationEngine {
                 throw e;
             }
         }
+    }
+
+    /**
+     * Overloaded method to accept Entities object
+     */
+    @Experimental(ExperimentalFeature.PARTIAL_EVALUATION)
+    @Override
+    public PartialAuthorizationResponse isAuthorizedPartial(com.cedarpolicy.model.PartialAuthorizationRequest q,
+                                                            PolicySet policySet, Entities entities) throws AuthException {
+        return isAuthorizedPartial(q, policySet, entities.getEntities());
     }
 
     @Override
