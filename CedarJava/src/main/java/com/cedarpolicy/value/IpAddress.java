4e4c55eae4910fee6f269cfc72bb6cd09548eabc
Mudit Chaudhary <chmudit@amazon.com>
2025-08-21 12:54:36 -0400

    adds support for CIDR suffix
    Signed-off-by: Mudit Chaudhary <chmudit@amazon.com>

--- CedarJava/src/main/java/com/cedarpolicy/value/IpAddress.java
+++ CedarJava/src/main/java/com/cedarpolicy/value/IpAddress.java
@@ -34,14 +34,15 @@ public class IpAddress extends Value {
         private static final Pattern IPV4_PATTERN =
                 Pattern.compile(
                         "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)"
-                                + "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+                                + "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+                                + "(?:/(?:[0-9]|[12][0-9]|3[0-2]))?$");
 
         public static boolean validIPv4(String ip) {
             if (ip == null || ip.isEmpty()) {
                 return false;
             }
             ip = ip.trim();
-            if ((ip.length() < 6) || (ip.length() > 15)) {
+            if ((ip.length() < 6) || (ip.length() > 18)) {
                 return false;
             }
             try {
@@ -54,21 +55,22 @@ public class IpAddress extends Value {
 
         private static final Pattern IPV6_PATTERN =
                 Pattern.compile(
-                        "^(([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|"
+                        "^((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|:))|"
                                 + "(([0-9A-Fa-f]{1,4}:)(((:[0-9A-Fa-f]{1,4}){1,6})|:))|"
-                                + "((:)(((:[0-9A-Fa-f]{1,4}){1,7})|:))|$");
+                                + "((:)(((:[0-9A-Fa-f]{1,4}){1,7})|:)))"
+                                + "(?:/(?:[0-9]|[1-9][0-9]|1[01][0-9]|12[0-8]))?$");
 
         public static boolean validIPv6(String ip) {
             if (ip == null || ip.isEmpty()) {
                 return false;
             }
             ip = ip.trim();
-            if ((ip.length() < 3) || (ip.length() > 39)) {
+            if ((ip.length() < 2) || (ip.length() > 43)) {
                 return false;
             }
             try {
